55a109971c822b648a06548853f8c280be39565c
NB Shashank <145538073+shashank27122002@users.noreply.github.com>
2026-09-02 16:13:56 +0200

    Time: 7 ms (27.90%) | Memory: 19.3 MB (74.38%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+
+                if board[i][j] == ".":
+                    continue
+
+                num = board[i][j]
+
+                # Check row
+                if num in rows[i]:
+                    return False
+                rows[i].add(num)
+
+                # Check column
+                if num in cols[j]:
+                    return False
+                cols[j].add(num)
+
+                # Find 3x3 box
+                box = (i // 3) * 3 + (j // 3)
+
+                if num in boxes[box]:
+                    return False
+                boxes[box].add(num)
+
+        return True
